c2dcd74053676d39faec7ab9283d665b5ef5a791
Sprogainiic <m.blinkens@gmail.com>
2019-09-23 23:26:55 +0300

    Add files via upload

---
+++ Lesson_5_Homework/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGetAll = new System.Windows.Forms.Button();
+            this.btnAddRecord = new System.Windows.Forms.Button();
+            this.btnDeleteRecords = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(230, 90);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(216, 116);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(49, 13);
+            this.lblSurname.TabIndex = 1;
+            this.lblSurname.Text = "Surname";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(403, 90);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "Age";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(387, 116);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(42, 13);
+            this.lblGender.TabIndex = 3;
+            this.lblGender.Text = "Gender";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(271, 87);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(271, 113);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 5;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(435, 85);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 6;
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Location = new System.Drawing.Point(435, 112);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(100, 21);
+            this.cmbGender.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(131, 223);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(546, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // btnGetAll
+            // 
+            this.btnGetAll.Location = new System.Drawing.Point(342, 389);
+            this.btnGetAll.Name = "btnGetAll";
+            this.btnGetAll.Size = new System.Drawing.Size(117, 23);
+            this.btnGetAll.TabIndex = 9;
+            this.btnGetAll.Text = "Get all records";
+            this.btnGetAll.UseVisualStyleBackColor = true;
+            this.btnGetAll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAddRecord
+            // 
+            this.btnAddRecord.Location = new System.Drawing.Point(330, 157);
+            this.btnAddRecord.Name = "btnAddRecord";
+            this.btnAddRecord.Size = new System.Drawing.Size(143, 23);
+            this.btnAddRecord.TabIndex = 10;
+            this.btnAddRecord.Text = "Add record";
+            this.btnAddRecord.UseVisualStyleBackColor = true;
+            this.btnAddRecord.Click += new System.EventHandler(this.btnAddRecord_Click);
+            // 
+            // btnDeleteRecords
+            // 
+            this.btnDeleteRecords.Location = new System.Drawing.Point(543, 389);
+            this.btnDeleteRecords.Name = "btnDeleteRecords";
+            this.btnDeleteRecords.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteRecords.TabIndex = 11;
+            this.btnDeleteRecords.Text = "Delete";
+            this.btnDeleteRecords.UseVisualStyleBackColor = true;
+            this.btnDeleteRecords.Click += new System.EventHandler(this.btnDeleteRecords_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDeleteRecords);
+            this.Controls.Add(this.btnAddRecord);
+            this.Controls.Add(this.btnGetAll);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGetAll;
+        private System.Windows.Forms.Button btnAddRecord;
+        private System.Windows.Forms.Button btnDeleteRecords;
+    }
+}
+
---
+++ Lesson_5_Homework/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string sqlConnectionString = @"Data Source=DESKTOP-DCIOL1B\SQLEXPRESS;Initial Catalog=C_Sharp_Exercise_DB;Integrated Security=True";
+
+        List<string> genderList = new List<string>() {"", "Male", "Female" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnDeleteRecords.Enabled = false;
+            cmbGender.DataSource = genderList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetAllRecordsFromDB();
+        }
+
+        private void btnAddRecord_Click(object sender, EventArgs e)
+        {
+
+            string sqlQueryString = $"INSERT INTO[C_Sharp_Exercise_DB].[dbo].[Persons] " +
+                                    $"([Name], [Surname], [Age], [Gender]) " +
+                                    $"VALUES('{txtName.Text}', '{txtSurname.Text}', {txtAge.Text}, '{cmbGender.SelectedItem}')";
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand(sqlQueryString, sqlConnection))
+                {
+                    sqlConnection.Open();
+                    try
+                    {
+                        sqlCommand.ExecuteNonQuery();
+                        GetAllRecordsFromDB();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Could not create table.");
+                    }
+                }
+            }
+        }
+
+        private void btnDeleteRecords_Click(object sender, EventArgs e)
+        {
+            var idToDelete = new List<int>();
+
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                {
+                    if (dataGridView1.Rows[row.Index].Cells[0].Value != null)
+                    {
+                        idToDelete.Add((int)dataGridView1.Rows[row.Index].Cells[0].Value);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                sqlConnection.Open();
+
+                foreach (var item in idToDelete)
+                {
+                    string sqlQueryString = $"DELETE FROM [C_Sharp_Exercise_DB].[dbo].[Persons] " +
+                                            $"WHERE [PersonID] = {item} ";
+                    using (SqlCommand sqlCommand = new SqlCommand(sqlQueryString, sqlConnection))
+                    {
+                        try
+                        {
+                            sqlCommand.ExecuteNonQuery();
+                            GetAllRecordsFromDB();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void GetAllRecordsFromDB()
+        {
+            string sqlQueryString = $"" +
+                                    $"SELECT PersonID, Name, Surname, Age, Gender " +
+                                    $"FROM dbo.Persons;";
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                var sqlDataAdapter = new SqlDataAdapter(sqlQueryString, sqlConnection);
+                var commandBuilder = new SqlCommandBuilder(sqlDataAdapter);
+                var dataSet = new DataSet();
+                sqlDataAdapter.Fill(dataSet);
+                dataGridView1.ReadOnly = true;
+                dataGridView1.DataSource = dataSet.Tables[0];
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                btnDeleteRecords.Enabled = true;
+            }
+            else
+            {
+                btnDeleteRecords.Enabled = false;
+            }
+        }
+    }
+}
